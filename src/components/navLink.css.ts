a39bf0130f1f8e41be049578fb582ae8d58163e1
Nathaniel Blackburn <support@nblackburn.uk>
2022-10-19 23:11:25 +0100

    feat: add navlink component

---
+++ src/components/navLink.css.ts
@@ -0,0 +1,31 @@
+import { style } from '@vanilla-extract/css';
+import { vars, sizes, primaryAccent } from '@styles/theme.css';
+
+export const navLink = style({
+    color: 'inherit',
+    fontWeight: 'bold',
+    textDecoration: 'none',
+    borderRadius: '9999px',
+    padding: `0.75rem ${sizes.small}`,
+    transition: 'border 0.2s ease-in-out',
+    border: `2px ${vars.colors.silverChalice} solid`,
+    selectors: {
+        '&:hover': {
+            borderColor: primaryAccent
+        }
+    },
+    '@media': {
+        '(prefers-color-scheme: dark)': {
+            borderColor: vars.colors.eerieBlack
+        }
+    }
+});
+
+export const navLinkActive = style({
+    borderColor: primaryAccent,
+    '@media': {
+        '(prefers-color-scheme: dark)': {
+            borderColor: primaryAccent
+        }
+    }
+});
---
+++ src/components/navLink.vue
@@ -0,0 +1,28 @@
+<template>
+    <a
+        :class="[styles.navLink, active ? styles.navLinkActive : '']"
+        :href="href"
+    >
+        <slot />
+    </a>
+</template>
+
+<script lang="ts">
+import * as styles from './navLink.css';
+
+export default {
+    props: {
+        href: {
+            type: String
+        },
+
+        active: {
+            type: Boolean
+        }
+    },
+
+    setup() {
+        return { styles };
+    }
+};
+</script>
