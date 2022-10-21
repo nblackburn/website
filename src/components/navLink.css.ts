14026af8e68d7420a0c5fe1b241ca1566a4558a0
Nathaniel Blackburn <support@nblackburn.uk>
2022-10-21 22:43:49 +0100

    style: center navLink content

--- src/components/navLink.css.ts
+++ src/components/navLink.css.ts
@@ -2,10 +2,13 @@ import { style } from '@vanilla-extract/css';
 import { vars, sizes, primaryAccent } from '@styles/theme.css';
 
 export const navLink = style({
+    display: 'flex',
     color: 'inherit',
     fontWeight: 'bold',
+    alignItems: 'center',
     textDecoration: 'none',
     borderRadius: '9999px',
+    justifyContent: 'center',
     padding: `0.75rem ${sizes.small}`,
     transition: 'border 0.2s ease-in-out',
     border: `2px ${vars.colors.silverChalice} solid`,
