27e4c4c4c0ef03c300b6ed18052e1c38fc2c8a9c
Nathaniel Blackburn <support@nblackburn.uk>
2022-10-23 17:17:30 +0100

    feat: added pageHeader component

---
+++ src/components/pageHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <div :class="styles.pageHeader">
+        <h1 :class="styles.title">{{ title }}</h1>
+        <p :class="styles.subTitle">{{ subTitle }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import * as styles from './pageHeader.css';
+
+export default {
+    props: {
+        title: {
+            type: String
+        },
+
+        subTitle: {
+            type: String
+        }
+    },
+
+    setup() {
+        return { styles };
+    }
+};
+</script>
