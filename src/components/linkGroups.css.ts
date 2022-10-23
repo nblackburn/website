0ba7b5b0efbd2d0d8c07efc952e0b86d7fe43678
Nathaniel Blackburn <support@nblackburn.uk>
2022-10-23 23:04:26 +0100

    feat: added linkGroups component

---
+++ src/components/linkGroups.css.ts
@@ -0,0 +1,52 @@
+import { style } from '@vanilla-extract/css';
+import { sizes, vars } from '@styles/theme.css';
+
+export const linkGroups = style({});
+
+export const linkGroup = style({
+    fontSize: '1.2rem',
+    marginBottom: sizes.medium,
+    borderRadius: sizes.small
+});
+
+export const heading = style({
+    fontWeight: 'bold',
+    marginBottom: sizes.small
+});
+
+export const items = style({
+    margin: 0,
+    padding: 0,
+    listStyle: 'none',
+    listStylePosition: 'inside'
+});
+
+export const item = style({
+    borderRadius: '0.25rem',
+    selectors: {
+        '&:nth-child(even)': {
+            background: vars.colors.silverChalice
+        }
+    },
+    '@media': {
+        '(prefers-color-scheme: dark)': {
+            selectors: {
+                '&:nth-child(even)': {
+                    background: vars.colors.eerieBlack
+                }
+            }
+        }
+    }
+});
+
+export const icon = style({
+    marginRight: sizes.small
+});
+
+export const link = style({
+    display: 'flex',
+    color: 'inherit',
+    alignItems: 'center',
+    textDecoration: 'none',
+    padding: '0.5rem 0.75rem'
+});
