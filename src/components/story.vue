1d7e2e67e58f804ad4c9d17aef466fd04cec5122
Nathaniel Blackburn <support@nblackburn.uk>
2022-10-26 20:38:42 +0100

    chore: improve about page

--- src/components/story.vue
+++ src/components/story.vue
@@ -4,30 +4,27 @@
             <slot name="image" />
         </div>
         <p :class="styles.paragraph">
-            I'm Nathaniel, it's nice to see a new face around here.
+            I'm Nathaniel. It's nice to see a new face around here.
         </p>
         <p :class="styles.paragraph">
-            I created this little place to share a little about what makes
-            me....well me. My story is perhaps a little different from the usual
-            as i am entirely self-taught. You see, i have always had a love for
-            computers. I started my journey at the age of 13 making little tools
-            for people.
+            My story is a little different from the usual, as I am self-taught.
+            You see, I have always had a love for computers. I started at 13
+            years old making little tools for people.
         </p>
         <p :class="styles.paragraph">
             Being a developer is not easy, especially when you are self-taught.
-            I have been faced so much rejection that it no longer has an effect
-            on me. But here is the thing, you have to keep going and use it as a
-            driving force. I simply wanted to be here enough that i kept on
-            fighting.
+            I have faced so much rejection that it no longer affects me. But
+            here's the thing. You have to keep going and use it as a driving
+            force. I wanted to be here enough that I kept on fighting.
         </p>
         <p :class="styles.paragraph">
-            My aim is simple, i love making people happy in any way i can. This
-            website is one of reflection to show off some of those things in the
-            hope that they can reach others.
+            My aim is simple, I love making people happy in any way I can. This
+            website is one of reflection to share some of those things I have
+            worked on in the hope that they can reach others.
         </p>
         <p :class="styles.paragraph">
-            Have a look around, stay a while and let's build something
-            beautiful....together.
+            Have a look around, stay a while, or reach out. I would love to hear
+            from you.
         </p>
     </div>
 </template>
