c3cb4371abe0cfa58c52f2f8cd4abadfa8563b87
Nathaniel Blackburn <support@nblackburn.uk>
2022-11-06 15:22:23 +0000

    style: tweak projectList styles

--- src/components/projectList.css.ts
+++ src/components/projectList.css.ts
@@ -2,8 +2,11 @@ import { sizes } from '@styles/theme.css';
 import { style } from '@vanilla-extract/css';
 
 export const projectList = style({
+    flex: 1,
     display: 'grid',
     rowGap: sizes.small,
     columnGap: sizes.small,
+    alignItems: 'flex-start',
+    marginBottom: sizes.medium,
     gridTemplateColumns: '1fr'
 });
