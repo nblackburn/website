ac634ff52fbdc4461886ec91e064f630bb39e406
Nathaniel Blackburn <support@nblackburn.uk>
2022-12-13 21:26:45 +0000

    style: add secondary accent to page header

--- src/components/pageHeader.css.ts
+++ src/components/pageHeader.css.ts
@@ -1,5 +1,6 @@
 import { style } from '@vanilla-extract/css';
-import { vars, sizes } from '@styles/theme.css';
+import { emerge } from '@styles/animations.css';
+import { vars, sizes, secondaryAccent } from '@styles/theme.css';
 
 export const pageHeader = style({
     fontSize: '1.2rem',
@@ -18,6 +19,28 @@ export const pageHeader = style({
 export const title = style({
     margin: '0 0 0.75rem 0',
     fontSize: sizes.medium
+});
+
+export const highlight = style({
+    position: 'relative',
+    color: secondaryAccent,
+    textShadow: '1px 1px 5px rgb(0 0 0 / 50%)',
+    selectors: {
+        '&:before': {
+            top: '50%',
+            left: '50%',
+            opacity: 0.1,
+            width: '500%',
+            height: '900%',
+            content: '\\0020',
+            filter: 'blur(30px)',
+            position: 'absolute',
+            borderRadius: '9999px',
+            background: 'currentcolor',
+            animation: `${emerge} 1s ease-in`,
+            transform: 'translate(-50%, -50%)'
+        }
+    }
 });
 
 export const subTitle = style({
