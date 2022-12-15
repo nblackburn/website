2adb8d01bad4bee3b6f1428f41d4259ab25c0a43
Nathaniel Blackburn <support@nblackburn.uk>
2022-12-15 19:04:09 +0000

    feat: add latest project component

---
+++ src/components/latestProject.css.ts
@@ -0,0 +1,11 @@
+import { sizes } from '@styles/theme.css';
+import { style } from '@vanilla-extract/css';
+
+export const latestProject = style({
+    position: 'relative'
+});
+
+export const title = style({
+    margin: '0 0 2rem 0',
+    fontSize: sizes.medium
+});
---
+++ src/components/latestProject.vue
@@ -0,0 +1,20 @@
+<template>
+    <section :class="styles.latestProject">
+        <h2 :class="styles.title">Latest project</h2>
+        <slot />
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import * as styles from './latestProject.css';
+import ProjectCard from '@components/projectCard.vue';
+
+export default defineComponent({
+    components: { ProjectCard },
+
+    setup() {
+        return { styles };
+    }
+});
+</script>
