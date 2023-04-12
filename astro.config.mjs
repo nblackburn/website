7329c2d54f9e8ad6da6bb073c3dae836ca273670
Nathaniel Blackburn <support@nblackburn.uk>
2023-04-12 19:43:18 +0100

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
 import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
 import vanillaExtract from 'astro-vanilla-extract';
 
 export default defineConfig({
+    output: 'static',
     site: 'https://nblackburn.uk/',
     integrations: [vue(), vanillaExtract(), sitemap()],
+
+    adapter: vercel({
+        analytics: true
+    }),
+
+    vite: {
+        define: {
+            'import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID': JSON.stringify(
+                process.env.VERCEL_ANALYTICS_ID
+            )
+        }
+    },
 
     experimental: {
         assets: true
