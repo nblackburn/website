1f1f40d574cb0924df076e2288d6f7e4b1786c07
Nathaniel Blackburn <support@nblackburn.uk>
2023-10-17 09:56:13 +0100

    style: remove accent background from header and hero

--- src/components/hero.css.ts
+++ src/components/hero.css.ts
@@ -1,5 +1,4 @@
 import { style } from '@vanilla-extract/css';
-import { emerge } from '@styles/animations.css';
 import { vars, sizes, primaryAccent, secondaryAccent } from '@styles/theme.css';
 
 export const hero = style({
@@ -30,24 +29,7 @@ export const highlight = style({
     textUnderlineOffset: '8px',
     textDecoration: 'underline',
     textDecorationStyle: 'wavy',
-    textDecorationThickness: 'from-font',
-    selectors: {
-        '&:before': {
-            zIndex: -1,
-            top: '50%',
-            left: '50%',
-            opacity: 0.1,
-            width: '300%',
-            height: '700%',
-            content: '\\0020',
-            filter: 'blur(30px)',
-            position: 'absolute',
-            borderRadius: '9999px',
-            background: 'currentcolor',
-            animation: `${emerge} 1s ease-in`,
-            transform: 'translate(-50%, -50%)'
-        }
-    }
+    textDecorationThickness: 'from-font'
 });
 
 export const primary = style({
--- src/components/pageHeader.css.ts
+++ src/components/pageHeader.css.ts
@@ -1,5 +1,4 @@
 import { style } from '@vanilla-extract/css';
-import { emerge } from '@styles/animations.css';
 import { vars, sizes, secondaryAccent } from '@styles/theme.css';
 
 export const pageHeader = style({
@@ -24,24 +23,7 @@ export const title = style({
 export const highlight = style({
     position: 'relative',
     color: secondaryAccent,
-    textShadow: '1px 1px 5px rgb(0 0 0 / 50%)',
-    selectors: {
-        '&:before': {
-            zIndex: -1,
-            top: '50%',
-            left: '50%',
-            opacity: 0.1,
-            width: '500%',
-            height: '900%',
-            content: '\\0020',
-            filter: 'blur(30px)',
-            position: 'absolute',
-            borderRadius: '9999px',
-            background: 'currentcolor',
-            animation: `${emerge} 1s ease-in`,
-            transform: 'translate(-50%, -50%)'
-        }
-    }
+    textShadow: '1px 1px 5px rgb(0 0 0 / 50%)'
 });
 
 export const subTitle = style({
