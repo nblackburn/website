830271cc85ec5c5b2d22aaecfe3f69f8e00ad823
Nathaniel Blackburn <support@nblackburn.uk>
2023-10-17 13:31:43 +0100

    style: add gradient to page body

--- src/components/main.css.ts
+++ src/components/main.css.ts
@@ -1,5 +1,10 @@
 import { style } from '@vanilla-extract/css';
-import { sizes, containers } from '@styles/theme.css';
+import {
+    sizes,
+    containers,
+    primaryAccent,
+    secondaryAccent
+} from '@styles/theme.css';
 
 export const main = style({
     flex: 1,
@@ -10,5 +15,17 @@ export const main = style({
     flexDirection: 'column',
     marginBottom: sizes.large,
     padding: `0 ${sizes.small}`,
-    maxWidth: containers.xtraLarge
+    maxWidth: containers.xtraLarge,
+    selectors: {
+        '&:before': {
+            zIndex: -1,
+            top: '0',
+            left: '0',
+            width: '100%',
+            height: '100vh',
+            content: '\\0020',
+            position: 'absolute',
+            background: `linear-gradient(180deg, rgba(${primaryAccent} / 0.1) 0%, rgba(${secondaryAccent} / 0.1) 75%, transparent 100%)`
+        }
+    }
 });
