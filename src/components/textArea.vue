ddc1e06490a2598b41dfb2f18911666955330e4e
Nathaniel Blackburn <support@nblackburn.uk>
2023-10-24 19:19:19 +0100

    feat: add textarea component

---
+++ src/components/textArea.vue
@@ -0,0 +1,39 @@
+<template>
+    <textarea
+        :type="type"
+        :name="name"
+        :id="id"
+        rows="5"
+        :required="required"
+        :class="styles.textArea"
+    />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import * as styles from './textArea.css';
+
+export default defineComponent({
+    props: {
+        type: {
+            type: String
+        },
+
+        name: {
+            type: String
+        },
+
+        id: {
+            type: String
+        },
+
+        required: {
+            type: Boolean
+        }
+    },
+
+    setup() {
+        return { styles };
+    }
+});
+</script>
