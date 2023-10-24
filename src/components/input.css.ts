1a6841f359bb105034bc1a8c4d4fc18af9b95545
Nathaniel Blackburn <support@nblackburn.uk>
2023-10-24 19:19:05 +0100

    feat: add input component

---
+++ src/components/input.css.ts
@@ -0,0 +1,25 @@
+import { style } from '@vanilla-extract/css';
+import { vars, sizes, primaryAccent } from '@styles/theme.css';
+
+export const input = style({
+    width: '100%',
+    fontSize: 'inherit',
+    padding: sizes.small,
+    borderRadius: sizes.tiny,
+    transition: 'all 0.2s ease-in-out',
+    boxShadow: `0px 0px 0px 0px transparent`,
+    border: `1px rgb(${vars.colors.silverChalice}) solid`,
+    selectors: {
+        '&:focus': {
+            borderColor: `rgb(${primaryAccent})`,
+            boxShadow: `0px 0px 0px 4px rgba(${primaryAccent} / 0.1)`
+        }
+    },
+    '@media': {
+        '(prefers-color-scheme: dark)': {
+            background: `rgba(0 0 0 / 0.3)`,
+            color: `rgb(${vars.colors.white})`,
+            border: `1px rgb(${vars.colors.eerieBlack}) solid`
+        }
+    }
+});
---
+++ src/components/input.vue
@@ -0,0 +1,38 @@
+<template>
+    <input
+        :type="type"
+        :name="name"
+        :id="id"
+        :required="required"
+        :class="styles.input"
+    />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import * as styles from './input.css';
+
+export default defineComponent({
+    props: {
+        type: {
+            type: String
+        },
+
+        name: {
+            type: String
+        },
+
+        id: {
+            type: String
+        },
+
+        required: {
+            type: Boolean
+        }
+    },
+
+    setup() {
+        return { styles };
+    }
+});
+</script>
