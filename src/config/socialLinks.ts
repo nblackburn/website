a26abe2437afd5ea71a0883c4a64fdead6a63a29
Nathaniel Blackburn <support@nblackburn.uk>
2023-10-25 19:39:45 +0100

    feat: added linkedin to social links

--- src/config/socialLinks.ts
+++ src/config/socialLinks.ts
@@ -7,6 +7,11 @@ export type SocialLink = {
 export const socialLinks: SocialLink[] = [
     { id: 'email', name: 'Email', href: 'mailto:support@nblackburn.uk' },
     {
+        id: 'linkedin',
+        name: 'LinkedIn',
+        href: 'https://linkedin.com/in/nblackburn'
+    },
+    {
         id: 'github',
         name: 'GitHub',
         href: 'https://github.com/nblackburn'
