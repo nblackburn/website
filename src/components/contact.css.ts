e6a28dd8ce6c396a1a66ce2f6736f850844d0d9e
Nathaniel Blackburn <support@nblackburn.uk>
2023-10-25 21:18:12 +0100

    feat: add contact section

---
+++ src/components/contact.css.ts
@@ -0,0 +1,24 @@
+import { style } from '@vanilla-extract/css';
+import { vars, sizes } from '@styles/theme.css';
+
+export const contact = style({
+    fontSize: '1.2rem'
+});
+
+export const title = style({
+    margin: `0 0 ${sizes.small} 0`
+});
+
+export const paragraph = style({
+    lineHeight: 1.3,
+    color: `rgb(${vars.colors.sonicSilver})`,
+    '@media': {
+        '(prefers-color-scheme: dark)': {
+            color: `rgb(${vars.colors.davyGrey})`
+        }
+    }
+});
+
+export const socialLinks = style({
+    marginTop: sizes.medium
+});
---
+++ src/components/contact.vue
@@ -0,0 +1,31 @@
+<template>
+    <section :class="styles.contact">
+        <h3 :class="styles.title">Let's keep in touch</h3>
+        <p :class="styles.paragraph">
+            Whether you are stuck with a bug, have a project you need help with
+            or just want to say hi, i'd love to hear from you.
+        </p>
+        <p :class="styles.paragraph">
+            Simply fill out the form or connect with me with one of the links
+            below.
+        </p>
+        <div :class="styles.socialLinks">
+            <SocialLinks :class="styles.socialLinks" />
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import * as styles from './contact.css';
+import NavLink from '@components/navLink.vue';
+import SocialLinks from '@components/socialLinks.vue';
+
+export default defineComponent({
+    components: { SocialLinks, NavLink },
+
+    setup() {
+        return { styles };
+    }
+});
+</script>
