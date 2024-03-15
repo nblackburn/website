d05ec6c127d12d31288b65090ca0fe0d46a61e67
Nathaniel Blackburn <support@nblackburn.uk>
2024-03-15 19:53:33 +0000

    chore: add vanilla extract vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-import vanillaExtract from 'astro-vanilla-extract';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 export default defineConfig({
     output: 'hybrid',
     compressHTML: true,
     site: 'https://nblackburn.uk/',
-    integrations: [vue(), vanillaExtract(), sitemap()],
+    integrations: [vue(), sitemap()],
 
     adapter: vercel({
         imageService: true,
@@ -21,6 +21,8 @@ export default defineConfig({
     }),
 
     vite: {
+        plugins: [vanillaExtractPlugin()],
+
         define: {
             'import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID': JSON.stringify(
                 process.env.VERCEL_ANALYTICS_ID
