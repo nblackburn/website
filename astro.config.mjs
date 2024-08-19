b53c27569a97a1221741b58a5ec1f86adf0f968c
Nathaniel Blackburn <support@nblackburn.uk>
2024-08-19 20:02:24 +0100

    feat: configure server

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,10 @@ export default defineConfig({
         mode: 'standalone'
     }),
 
+    server: {
+        host: '0.0.0.0'
+    },
+
     vite: {
         plugins: [vanillaExtractPlugin()]
     }
