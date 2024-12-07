461e95cfc85e097e819c76e14379f630c0565342
Nathaniel Blackburn <support@nblackburn.uk>
2024-12-07 18:52:34 +0000

    style: tweak navLink padding

--- src/components/navLink.css.ts
+++ src/components/navLink.css.ts
@@ -5,12 +5,12 @@ export const navLink = style({
     display: 'flex',
     color: 'inherit',
     fontWeight: 'bold',
+    padding: sizes.mini,
     alignItems: 'center',
     textDecoration: 'none',
     borderRadius: '9999px',
     justifyContent: 'center',
     border: '1px transparent solid',
-    padding: `0.75rem ${sizes.small}`,
     transition: 'border 0.2s ease-in-out'
 });
 
