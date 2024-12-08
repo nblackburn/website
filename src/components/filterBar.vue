44db46c2f6fca20a9e41ef412633a14475b5c1ba
Nathaniel Blackburn <support@nblackburn.uk>
2024-12-08 11:19:59 +0000

    feat: added filter bar component

---
+++ src/components/filterBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div :class="styles.filterBar">
+        <ol :class="styles.items">
+            <li
+                :class="[
+                    styles.item,
+                    tag === active ? styles.activeItem : undefined
+                ]"
+                v-for="tag in tags"
+                v-bind:key="tag"
+            >
+                <a :class="[styles.link]" :href="'/projects/tags/' + tag"
+                    >#{{ tag }}</a
+                >
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import * as styles from './filterBar.css';
+
+interface Props {
+    tags?: string[];
+    active?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+    tags: () => [],
+    active: undefined
+});
+</script>
