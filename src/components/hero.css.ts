610dbf5cba7a9942ec2e53208d8bf6ca6a31fc7e
Nathaniel Blackburn <support@nblackburn.uk>
2024-12-10 21:17:57 +0000

    feat: animate hero strapline

--- src/components/hero.css.ts
+++ src/components/hero.css.ts
@@ -1,5 +1,5 @@
-import { style } from '@vanilla-extract/css';
-import { sizes, primaryAccent, secondaryAccent } from '@styles/theme.css';
+import { keyframes, style } from '@vanilla-extract/css';
+import { vars, sizes, primaryAccent, secondaryAccent } from '@styles/theme.css';
 
 export const hero = style({
     display: 'flex',
@@ -18,14 +18,32 @@ export const strapLine = style({
     fontWeight: 'normal'
 });
 
-export const highlight = style({
-    fontWeight: 'bold'
+const animatedAccent = keyframes({
+    '0%': { backgroundPositionX: '0%' },
+    '25%': { backgroundPositionX: '100%' },
+    '50%': { backgroundPositionX: '100%' },
+    '100%': { backgroundPositionX: '0%' }
 });
 
-export const primary = style({
-    color: `rgb(${primaryAccent})`
+export const accent = style({
+    backgroundSize: '200%',
+    backgroundClip: 'text',
+    WebkitBackgroundClip: 'text',
+    color: 'rgba(255 255 255 / 5%)',
+    animation: `${animatedAccent} 5s ease-in-out infinite`,
+    backgroundImage: `radial-gradient(circle, rgba(${primaryAccent} / 95%) 0%, rgba(${secondaryAccent} / 95%) 100%)`,
+    '@media': {
+        '(prefers-reduced-motion)': {
+            animation: 'none'
+        }
+    }
 });
 
-export const secondary = style({
-    color: `rgb(${secondaryAccent})`
+export const reset = style({
+    color: `rgb(${vars.colors.eerieBlack})`,
+    '@media': {
+        '(prefers-color-scheme: dark)': {
+            color: '#ffffff'
+        }
+    }
 });
