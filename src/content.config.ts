713413f5a39dfca1d2e03bb7b3ed523663dd1786
Nathaniel Blackburn <support@nblackburn.uk>
2024-12-18 22:42:30 +0000

    feat: setup schema for experience

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,10 @@
+import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
+const jobDateRegexp = /[0-9]{4}-[0-9]{2}/;
+
 const project = defineCollection({
+    loader: glob({ pattern: '**/*.md', base: './src/content/project' }),
     schema: ({ image }) =>
         z.object({
             title: z.string(),
@@ -12,6 +16,22 @@ const project = defineCollection({
         })
 });
 
+const experience = defineCollection({
+    loader: glob({ pattern: 'experience.json', base: './src/content' }),
+    schema: z.array(
+        z.object({
+            startDate: z.string().regex(jobDateRegexp),
+            endDate: z.nullable(z.string().regex(jobDateRegexp)),
+            jobTitle: z.string(),
+            company: z.object({
+                name: z.string(),
+                url: z.nullable(z.string().url())
+            })
+        })
+    )
+});
+
 export const collections = {
-    project
+    project,
+    experience
 };
