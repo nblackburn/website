70f475cefad4cde61cd378c05444097f660926b1
Nathaniel Blackburn <support@nblackburn.uk>
2025-01-03 23:38:55 +0000

    chore: configure node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
+import node from '@astrojs/node';
 
 export default defineConfig({
     compressHTML: true,
     site: 'https://nblackburn.uk/',
     integrations: [vue(), sitemap()],
 
-    adapter: vercel({
-        imageService: true
+    adapter: node({
+        mode: 'standalone'
     }),
 
     vite: {
