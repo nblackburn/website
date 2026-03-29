40e738853e7c00945d3969633986ed896fc00ce3
Nathaniel Blackburn <support@nblackburn.uk>
2026-03-29 22:16:59 +0100

    feat: add policies to footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,9 +1,23 @@
 <template>
     <footer :class="styles.footer">
         <div :class="styles.container">
-            <div :class="styles.copyright">
-                &copy; {{ currentYear }} Nathaniel Blackburn. All Rights
-                Reserved.
+            <div :class="styles.legal">
+                <div :class="styles.copyright">
+                    &copy; {{ currentYear }} Nathaniel Blackburn. All Rights
+                    Reserved.
+                </div>
+                <ol :class="styles.items">
+                    <li :class="styles.item">
+                        <a :class="styles.link" href="/privacy-policy"
+                            >Privacy policy</a
+                        >
+                    </li>
+                    <li :class="styles.item">
+                        <a :class="styles.link" href="/terms-of-service"
+                            >Terms of service</a
+                        >
+                    </li>
+                </ol>
             </div>
             <div :class="styles.version">v{{ version }}</div>
         </div>
